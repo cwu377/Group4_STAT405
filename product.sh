f13e4dc59d001fbc403597f89c317ef90e5e7f70
cwu377 <cwu377@learn.chtc.wisc.edu>
2023-04-27 11:58:35 -0500

    add product.sh

---
+++ product.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# untar your R installation
+tar -xzf R413.tar.gz
+tar -xzf packages_FITSio_tidyverse.tar.gz
+
+# make sure the script will use your R installation, 
+# and the working directory as its home location
+export PATH=$PWD/R/bin:$PATH
+export RHOME=$PWD/R
+export R_LIBS=$PWD/packages
+
+# run your script
+Rscript read_data.R $1
+

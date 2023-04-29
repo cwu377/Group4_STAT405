868790b829969961022abf8f37b20549efabd482
cwu377 <cwu377@learn.chtc.wisc.edu>
2023-04-29 17:27:07 -0500

    loading packages

--- product.sh
+++ product.sh
@@ -2,9 +2,11 @@
 
 # untar your R installation
 tar -xzf R413.tar.gz
-tar -xzf packages_FITSio_tidyverse.tar.gz
-tar -xzf tidytext_0.4.1.tar.gz
-tar -xzf sentimentr_2.9.0.tar.gz
+tar -xzf packages.tar.gz
+#tar -xzf packages_FITSio_tidyverse.tar.gz
+
+#tar -xzf tidytext_0.4.1.tar.gz
+#tar -xzf sentimentr_2.9.0.tar.gz
 
 # make sure the script will use your R installation, 
 # and the working directory as its home location
@@ -13,5 +15,5 @@ export RHOME=$PWD/R
 export R_LIBS=$PWD/packages
 
 # run your script
-Rscript read_data.R $1
+Rscript semanticsProject.R $1
 
